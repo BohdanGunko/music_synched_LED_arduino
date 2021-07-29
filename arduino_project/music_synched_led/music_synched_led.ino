8f4ad88ab66da01bd169168cf0e08ab4daf8ca98
BohdanHunko <Bohdan.Hunko@infineon.com>
2021-07-29 23:34:54 +0300

    FastLED test app added

--- arduino_project/music_synched_led/music_synched_led.ino
+++ arduino_project/music_synched_led/music_synched_led.ino
@@ -1,9 +1,31 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
+#include <FastLED.h>
 
+#define NUM_LEDS  30
+#define LED_PIN   22
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(255, 0, 0);
+  }
+  FastLED.show();
+  delay(1000);
 
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(0, 255, 0);
+  }
+  FastLED.show();
+  delay(1000);
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB(0, 0, 255);
+  }
+  FastLED.show();
+  delay(1000);
 }
