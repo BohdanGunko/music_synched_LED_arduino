32f3b6a3823cc2addd89750dc84350579ae1ca4d
BohdanHunko <Bohdan.Hunko@infineon.com>
2021-07-30 00:05:05 +0300

    Wi-Fi disabled

--- arduino_project/music_synched_led/music_synched_led.ino
+++ arduino_project/music_synched_led/music_synched_led.ino
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <esp_wifi.h>
 
 #define NUM_LEDS  30
 #define LED_PIN   22
@@ -8,6 +9,7 @@ CRGB leds[NUM_LEDS];
 
 void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  esp_wifi_stop();
 }
 
 void loop() {
@@ -21,7 +23,7 @@ void loop() {
     leds[i] = CRGB(0, 255, 0);
   }
   FastLED.show();
-  delay(1000);
+  vTaskDelay(1000);
 
   for (int i = 0; i < NUM_LEDS; i++) {
     leds[i] = CRGB(0, 0, 255);
