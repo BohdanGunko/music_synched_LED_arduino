482dd0504cc0291e13d26cc6e20d66d9532e0d49
BohdanHunko <Bohdan.Hunko@infineon.com>
2021-08-08 20:51:50 +0300

    task reworked to take advantage ofmulticore topology

--- arduino_project/music_synched_led/music_synched_led.ino
+++ arduino_project/music_synched_led/music_synched_led.ino
@@ -13,29 +13,31 @@
 BluetoothSerial SerialBT;
 CRGB leds[NUM_LEDS];
 
-//void bluetooth_recieve (void*) {
-//  if (SerialBT.available()) {
-//    Serial.write(SerialBT.read());
-//  }
-//  vTaskDelay(30);
-//}
+void bluetooth_recieve (void*) {
+  while(true){
+    if (SerialBT.available()) {
+      Serial.write(SerialBT.read());
+  }
+  vTaskDelay(30);
+  }
+}
 
-void leds_blink(void* params) {
+void leds_blink(void*) {
   while (true) {
     for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(255, 0, 0);
+      leds[i] = CRGB(25, 0, 0);
     }
     FastLED.show();
     vTaskDelay(1000);
 
     for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(0, 255, 0);
+      leds[i] = CRGB(0, 25, 0);
     }
     FastLED.show();
     vTaskDelay(1000);
 
     for (int i = 0; i < NUM_LEDS; i++) {
-      leds[i] = CRGB(0, 0, 255);
+      leds[i] = CRGB(0, 0, 25);
     }
     FastLED.show();
     vTaskDelay(1000);
@@ -47,7 +49,7 @@ void setup() {
   Serial.println("The device started, now you can pair it with bluetooth!");
 
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  //  esp_wifi_stop();
+   esp_wifi_stop();
 
   xTaskCreatePinnedToCore(
     leds_blink,       //Function to implement the task
@@ -58,15 +60,15 @@ void setup() {
     NULL,       //Task handle.
     1);         //Core where the task should run
 
-  //  xTaskCreatePinnedToCore(
-  //    bluetooth_recieve,       //Function to implement the task
-  //    "bluetooth_recieve", //Name of the task
-  //    6000,       //Stack size in words
-  //    NULL,       //Task input parameter
-  //    0,          //Priority of the task
-  //    NULL,       //Task handle.
-  //    0);         //Core where the task should run
-  //  vTaskStartScheduler();
+   xTaskCreatePinnedToCore(
+     bluetooth_recieve,       //Function to implement the task
+     "bluetooth_recieve", //Name of the task
+     6000,       //Stack size in words
+     NULL,       //Task input parameter
+     0,          //Priority of the task
+     NULL,       //Task handle.
+     0);         //Core where the task should run
+   vTaskStartScheduler();
 }
 
 
